e2f1f7416daa59aace59f2c0f7ea68792fd3c2ba
draca3565 <troyknife@gmail.com>
2017-06-24 08:50:05 -0600

    5/10/17-Initial Commit to repository Added more classes 5/25 calcAmmo added pulled classes 6/15 added super view class

--- Hunt/src/hunt/Hunt.java
+++ Hunt/src/hunt/Hunt.java
@@ -1,7 +1,7 @@
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * and open the template in the editor. 
  */
 package hunt;
 import byui.cit260.theHunt.model.Actor;
